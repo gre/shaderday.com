a7d46a7e770652808856795dbd76597b6d6881c0
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2021-05-16 17:44:40 +0200

    up

---
+++ _next/static/09XfB92MbNTgmVQAAp0C-/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fposts","\u002F","\u002Fplots\u002F[n]","\u002F[year]\u002F[month]\u002F[slug]","\u002F[year]","\u002Fshaderday\u002F[day]","\u002Fexport\u002F[day]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
