5464b964bf84bf874686d7811fd24f03f00bd291
gre <gre@users.noreply.github.com>
2021-07-12 18:29:38 +0000

    deploy: bf0a62e8664c9ce1cb9287b9d86c5982fb469dfb

---
+++ _next/static/chunks/39.bdbbdc33ffb2f35517e4.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp_N_E=window.webpackJsonp_N_E||[]).push([[39],{GZd6:function(n,i,t){"use strict";t.r(i),i.default='---\ntitle: "Mountain moons"\nthumbnail: /images/plots/190.jpg\ndescription: "Continuation on packing circles with layered lines."\ntags:\n  - shape-packing\n---\n\nContinuation on packing circles with layered lines.'}}]);
